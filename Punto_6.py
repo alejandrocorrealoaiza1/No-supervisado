f575e3de165ae86982cae10a0af95fef8e97e664
alejandrocorrealoaiza1 <alejandro.correa.loaiza1@gmail.com>
2023-04-15 04:18:34 -0500

    versión 2.5

--- Punto_6.py
+++ Punto_6.py
@@ -1,48 +1,39 @@
 
-from sklearn.datasets import fetch_openml
-from sklearn.model_selection import train_test_split
 from keras.datasets import mnist
-from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 from Package.PCA import PCA
 import matplotlib.pyplot as plt
-import numpy as np
-
+import seaborn as sns
 
 # CARGAR EL DATASET
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 
 # DISTINGUIR ENTRE UNOS Y OCHOS
-X_train_08 = X_train[(y_train == '0') | (y_train == '8')]/255 
-y_train_08 = y_train[(y_train == '0') | (y_train == '8')]
+X_train = X_train[(y_train==0)|(y_train==8)]
+y_train_ = y_train[(y_train==0)|(y_train==8)]
+X_test = X_test[(y_test==0)|(y_test==8)]
+y_test = y_test[(y_test==0)|(y_test==8)]
 
-X_test_08 = X_test[(y_test == '0') | (y_test == '8')]/255 
-y_test_08 = y_test[(y_test == '0') | (y_test == '8')]
 
-X_train_08 = X_train_08.reshape(X_train_08.shape[0] , X_train_08.shape[1]* X_train_08.shape[2])   
-X_test_08 = X_test_08.reshape(X_test_08.shape[0] , X_test_08.shape[1]* X_test_08.shape[2])  
+#EL DATASET ES DE TRES DIMENSIONES, SE PASA A 2D
+X_test = X_test.reshape(X_test.shape[0],X_test.shape[1]*X_test.shape[2])
 
-ids1 = np.random.randint(0,60000, (3000))
-X_train_s = X_train[ids1]
-y_train_s = y_train[ids1]
 
-ids2 = np.random.randint(0,10000, (100))
-X_test_s = X_test[ids2]
-y_test_s = y_test[ids2]
+#SE ESCALAN LOS DATOS
+x_test = StandardScaler().fit_transform(X_test)
 
-# UTILIZAR EL MÉTODO PCA
+#MODELO PCA
 pca = PCA(n_components=2)
-pca.fit_transform(X_train_s)
-X_08_pca = pca.transform(X_test_s)
+X_test_PCA = pca.fit_transform(X_test)
 
 
+#GRAFICAR PCA
+fig,ax=plt.subplots(1,1,figsize=(9,9))
+sns.scatterplot(x = X_test_PCA[:,0]
+                , y = X_test_PCA[:,1]
+                , hue = y_test
+                , palette = 'Paired'
+                , legend = 'full')
+plt.show()
 
-# CRAR EL MODELO
-#model = LogisticRegression()
-
-# ENTRENAR EL MODELO
-#model.fit(X_08_pca, y_train_08)
-
-# VALIDACIÓN DEL MODELO
-#accuracy = model.score(X_test_08, y_test_08)
-# print(accuracy)
